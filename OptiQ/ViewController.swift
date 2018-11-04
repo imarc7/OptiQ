50e676c3194857881a81020e1fe7b956efff2d83
Marc Ibrahim <marcibrahim@marcs-macbook-pro.local>
2018-11-04 09:40:34 +0200

    Initial Commit

---
+++ OptiQ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OptiQ
+//
+//  Created by Marc Ibrahim on 11/4/18.
+//  Copyright © 2018 Marc Ibrahim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
